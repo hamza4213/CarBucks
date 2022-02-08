4767a96889d643acccf3595a9aeaa17b1c4a6adb
hamza4213 <hamzashabbir4213@gmail.com>
2022-02-08 14:19:34 +0500

    working

--- source/utils/endPoint.js
+++ source/utils/endPoint.js
@@ -1,18 +1,12 @@
-// const baseURL = 'https://api.autofixz.com/v1/';
-const baseURL = 'http://192.168.18.98:4000/v1/';
+const baseURL = 'https://api.autofixz.com/v1/';
+// const baseURL = 'http://192.168.18.98:4000/v1/';
 const SOCKET_URL = 'https://api.autofixz.com/';
 
-
 const paths = {
-    baseURL,
-    getCategories: baseURL + "categories/getCategoriesPublic",
-    getSubCategory: baseURL + "categories/getSubCategoriesPublic/",
-    getPromotions: baseURL + "promotions",
-}
+  baseURL,
+  getCategories: baseURL + 'categories/getCategoriesPublic',
+  getSubCategory: baseURL + 'categories/getSubCategoriesPublic/',
+  getPromotions: baseURL + 'promotions',
+};
 
-
-export {
-    baseURL,
-    SOCKET_URL,
-    paths
-}
+export {baseURL, SOCKET_URL, paths};
