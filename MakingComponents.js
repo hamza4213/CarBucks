56c7a640d2392f0eab9f38111504ed008a0d7cb8
hamza4213 <hamzashabbir4213@gmail.com>
2022-02-12 14:10:07 +0500

    InsuranceComponentComplete

--- MakingComponents.js
+++ MakingComponents.js
@@ -1,20 +1,22 @@
-import {ScrollView, Text} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import SmallImage from './source/common/components/ImagesComponents/SmallImage';
 import LargeImage from './source/common/components/ImagesComponents/LargeImage';
 import SmallImageForFlatlist from './source/common/components/ImagesComponents/SmallImageForFlatlist';
 import WorkShopDetails from './source/common/components/WorkshopDetailsComponent.js/WorkShopDetails';
 import AnalyticsComponent from './source/common/components/AnalyticsComponent/AnalyticsComponent';
+import InsuranceComponent from './source/common/components/InsuranceComponent/InsuranceComponent';
 
 const MakingComponents = () => {
   return (
-    <ScrollView style={{backgroundColor: 'white'}}>
-      <SmallImage />
+    <View style={{backgroundColor: 'white', flex: 1}}>
+      {/* <SmallImage />
       <LargeImage />
       <SmallImageForFlatlist />
       <WorkShopDetails />
-      <AnalyticsComponent />
-    </ScrollView>
+      <AnalyticsComponent /> */}
+      <InsuranceComponent />
+    </View>
   );
 };
 
---
+++ source/common/components/InsuranceComponent/InsuranceComponent.js
@@ -0,0 +1,87 @@
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import colors, {
+  primaryDark,
+  primaryLight,
+  darkOrange,
+  primary,
+  text,
+} from '../../../common/constants/colors';
+import LinearGradientWrapper from '../../../../source/common/components/LinearGradientWrapper/index';
+const InsuranceComponent = props => {
+  return (
+    <View
+      style={{
+        width: '90%',
+        height: 120,
+        backgroundColor: '#fff',
+        marginTop: 20,
+        // alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: 10,
+        shadowOffset: {width: 100, height: 50},
+        shadowColor: '#000',
+        shadowOpacity: 1,
+        elevation: 15,
+        flexDirection: 'row',
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '35%',
+          height: '100%',
+          justifyContent: 'center',
+          //   backgroundColor: 'yellow',
+          alignItems: 'center',
+        }}>
+        <Image
+          style={{width: '90%', height: '90%', borderRadius: 15}}
+          source={require('../../../assets/pngs/buyCarImage.jpg')}
+        />
+      </View>
+      <View
+        style={{
+          //   flexDirection: 'row',
+          width: '65%',
+          height: '100%',
+
+          //   backgroundColor: 'teal',
+          //   alignItems: 'center',
+          paddingHorizontal: 10,
+        }}>
+        <View style={{height: 10}}></View>
+        <Text style={{color: primary, fontWeight: 'bold'}}>
+          Tata Aig insurance
+        </Text>
+        <Text style={{color: text}}>
+          Lorem Ipsum is simply dummy text of the printing.
+        </Text>
+        <View style={{height: 10}}></View>
+        <LinearGradientWrapper
+          style={{
+            width: '50%',
+            height: 30,
+            borderRadius: 20,
+            alignSelf: 'flex-end',
+            marginRight: 10,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          right={true}>
+          <TouchableOpacity
+            style={{
+              height: '100%',
+              width: '100%',
+
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text style={{color: 'white'}}>Explore Now</Text>
+          </TouchableOpacity>
+        </LinearGradientWrapper>
+      </View>
+    </View>
+  );
+};
+
+export default InsuranceComponent;
